f267aa78d133ef6845d74c613ab054d04e2c50c6
Jérémy Sorant <jsorant@gmail.com>
2024-01-08 10:47:21 +0100

    Apply spring-boot module

---
+++ src/main/java/com/jsorant/enigma/backend/EnigmaBackendJavaApp.java
@@ -0,0 +1,24 @@
+package com.jsorant.enigma.backend;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import com.jsorant.enigma.backend.shared.generation.domain.ExcludeFromGeneratedCodeCoverage;
+
+@SpringBootApplication
+@ExcludeFromGeneratedCodeCoverage(reason = "Not testing logs")
+public class EnigmaBackendJavaApp {
+
+  private static final Logger log = LoggerFactory.getLogger(EnigmaBackendJavaApp.class);
+
+  public static void main(String[] args) {
+    Environment env = SpringApplication.run(EnigmaBackendJavaApp.class, args).getEnvironment();
+
+    if (log.isInfoEnabled()) {
+      log.info(ApplicationStartupTraces.of(env));
+    }
+  }
+}
