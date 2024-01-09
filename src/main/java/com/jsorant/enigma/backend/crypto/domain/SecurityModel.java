ac8baa6a0d67a931ff0c65f9e814562fc1f170c0
Jérémy Sorant <jsorant@gmail.com>
2024-01-09 15:28:32 +0100

    Added SecurityModel

--- src/main/java/com/jsorant/enigma/backend/crypto/domain/SecurityModel.java
+++ src/main/java/com/jsorant/enigma/backend/crypto/domain/SecurityModel.java
@@ -1,24 +1,46 @@
 package com.jsorant.enigma.backend.crypto.domain;
 
 import com.jsorant.enigma.backend.shared.error.domain.Assert;
+import java.util.ArrayList;
 
 public class SecurityModel {
 
   private final String name;
 
-  public SecurityModel(String name) {
+  private final ArrayList<Engine> engines;
+
+  private SecurityModel(String name, ArrayList<Engine> engines) {
     Assert.notBlank("name", name);
 
     this.name = name;
+    this.engines = engines;
   }
 
   public static SecurityModelBuilder builder() {
     return new SecurityModelBuilder();
   }
 
+  public String encrypt(String input) {
+    String result = input;
+    for (Engine engine : engines) {
+      result = engine.encrypt(result);
+    }
+    return result;
+  }
+
+  public String decrypt(String input) {
+    String result = input;
+    for (Engine engine : engines.reversed()) {
+      result = engine.decrypt(result);
+    }
+    return result;
+  }
+
   public static class SecurityModelBuilder {
 
     private String name;
+
+    private final ArrayList<Engine> engines = new ArrayList<>();
 
     private SecurityModelBuilder() {}
 
@@ -27,8 +49,13 @@ public class SecurityModel {
       return this;
     }
 
+    public SecurityModelBuilder withEngine(Engine engine) {
+      engines.add(engine);
+      return this;
+    }
+
     public SecurityModel build() {
-      return new SecurityModel(this.name);
+      return new SecurityModel(this.name, this.engines);
     }
   }
 }
--- src/test/java/com/jsorant/enigma/backend/crypto/domain/SecurityModelTest.java
+++ src/test/java/com/jsorant/enigma/backend/crypto/domain/SecurityModelTest.java
@@ -1,11 +1,18 @@
 package com.jsorant.enigma.backend.crypto.domain;
 
-import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
 
 import com.jsorant.enigma.backend.UnitTest;
 import com.jsorant.enigma.backend.shared.error.domain.MissingMandatoryValueException;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 
 @UnitTest
 public class SecurityModelTest {
@@ -22,7 +29,107 @@ public class SecurityModelTest {
   }
 
   @Test
-  void shouldBuildWithAnEngine() {
-    //SecurityModel.builder().name("").withEngine(new DummyEngine).build();
+  void shouldBuildWithMultipleEngines() {
+    assertDoesNotThrow(() ->
+      SecurityModel
+        .builder()
+        .name("SecurityModelTest")
+        .withEngine(new Caesar(1, 1))
+        .withEngine(new Rotor("BDFHJLCPRTXVZNYEIWGAKMUSQO"))
+        .build()
+    );
+  }
+
+  @Test
+  void shouldEncryptWithoutEngines() {
+    SecurityModel securityModel = SecurityModel.builder().name("SecurityModelTest").build();
+
+    assertThat(securityModel.encrypt("AAA")).isEqualTo("AAA");
+  }
+
+  @ParameterizedTest(name = "Encrypt {0} with shift {1}, increment {2}, rotor {3}, rotor {4} and rotor {5} should return {6}")
+  @ArgumentsSource(SecurityModelTest.EncryptArgumentsProvider.class)
+  void shouldEncrypt(String input, int shift, int increment, String rotor1, String rotor2, String rotor3, String expectedResult) {
+    SecurityModel securityModel = SecurityModel
+      .builder()
+      .name("SecurityModelTest")
+      .withEngine(new Caesar(shift, increment))
+      .withEngine(new Rotor(rotor1))
+      .withEngine(new Rotor(rotor2))
+      .withEngine(new Rotor(rotor3))
+      .build();
+
+    assertThat(securityModel.encrypt(input)).isEqualTo(expectedResult);
+  }
+
+  static class EncryptArgumentsProvider implements ArgumentsProvider {
+
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) {
+      return Stream.of(
+        Arguments.of("AAA", 4, 1, "BDFHJLCPRTXVZNYEIWGAKMUSQO", "AJDKSIRUXBLHWTMCQGZNPYFVOE", "EKMFLGDQVZNTOWYHXUSPAIBRCJ", "KQF"),
+        Arguments.of(
+          "EVERYONEISWELCOMEHERE",
+          9,
+          1,
+          "BDFHJLCPRTXVZNYEIWGAKMUSQO",
+          "AJDKSIRUXBLHWTMCQGZNPYFVOE",
+          "EKMFLGDQVZNTOWYHXUSPAIBRCJ",
+          "PQSACVVTOISXFXCIAMQEM"
+        ),
+        Arguments.of(
+          "EVERYONEISWELCOMEHEREEVERYONEISWELCOMEHERE",
+          9,
+          3,
+          "BDFHTXVZNYEIWGAKMUSQOJLCPR",
+          "AJDKSIRUXBGZNPYFVOELHWTMCQ",
+          "TOWYHXUSPAIBRCJEKMFLGDQVZN",
+          "MKDWDLTEUPWZBXMTWUUROXHBZBYJDAMZRUWXJZAACQ"
+        )
+      );
+    }
+  }
+
+  @ParameterizedTest(name = "Decrypt {0} with shift {1}, increment {2}, rotor {3}, rotor {4} and rotor {5} should return {6}")
+  @ArgumentsSource(SecurityModelTest.DecryptArgumentsProvider.class)
+  void shouldDecrypt(String input, int shift, int increment, String rotor1, String rotor2, String rotor3, String expectedResult) {
+    SecurityModel securityModel = SecurityModel
+      .builder()
+      .name("SecurityModelTest")
+      .withEngine(new Caesar(shift, increment))
+      .withEngine(new Rotor(rotor1))
+      .withEngine(new Rotor(rotor2))
+      .withEngine(new Rotor(rotor3))
+      .build();
+
+    assertThat(securityModel.decrypt(input)).isEqualTo(expectedResult);
+  }
+
+  static class DecryptArgumentsProvider implements ArgumentsProvider {
+
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) {
+      return Stream.of(
+        Arguments.of("KQF", 4, 1, "BDFHJLCPRTXVZNYEIWGAKMUSQO", "AJDKSIRUXBLHWTMCQGZNPYFVOE", "EKMFLGDQVZNTOWYHXUSPAIBRCJ", "AAA"),
+        Arguments.of(
+          "PQSACVVTOISXFXCIAMQEM",
+          9,
+          1,
+          "BDFHJLCPRTXVZNYEIWGAKMUSQO",
+          "AJDKSIRUXBLHWTMCQGZNPYFVOE",
+          "EKMFLGDQVZNTOWYHXUSPAIBRCJ",
+          "EVERYONEISWELCOMEHERE"
+        ),
+        Arguments.of(
+          "MKDWDLTEUPWZBXMTWUUROXHBZBYJDAMZRUWXJZAACQ",
+          9,
+          3,
+          "BDFHTXVZNYEIWGAKMUSQOJLCPR",
+          "AJDKSIRUXBGZNPYFVOELHWTMCQ",
+          "TOWYHXUSPAIBRCJEKMFLGDQVZN",
+          "EVERYONEISWELCOMEHEREEVERYONEISWELCOMEHERE"
+        )
+      );
+    }
   }
 }
